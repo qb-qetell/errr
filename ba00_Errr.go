cd81cf8c51d62ddeb05c74c3e5763488bac214db
Brian Ibrahim Qamardeen <brian@qeetell.eon>
2022-07-14 13:46:50 +0100

    Ds progress

--- ba00_Errr.go
+++ ba00_Errr.go
@@ -6,21 +6,21 @@ import (
 type Errr struct {
 	Iddd string
 	Dtll string
-	csss error
+	Csss error
 }
 	func Errr_Estb (iddd, dtll string, csss ... error) (*Errr) {
 		errr := &Errr {}
 		errr.Iddd = iddd
 		errr.Dtll = dtll
 		if len (csss) > 0 {
-			errr.csss = csss [0]
+			errr.Csss = csss [0]
 		}
 		return errr
 	}
 	func (objc *Errr) Error () (string) {
 		dtll := objc.Dtll
-		if objc.csss != nil {
-			dtll = fmt.Sprintf ("%s [%s]", dtll, objc.csss.Error ())
+		if objc.Csss != nil {
+			dtll = fmt.Sprintf ("%s [%s]", objc.Dtll, objc.Csss.Error ())
 		}
 		return dtll
 	}
